a27b783bd4c8a868c18ccc0195b83bc5a07f4313
chaos <chaos@localhost.localdomain>
2018-02-26 03:02:31 -0500

    mosaico advances

--- test.py
+++ test.py
@@ -14,10 +14,26 @@ def mosaico(imgname,tamx,tamy):
 			box = (xant,yant,xant+tamx,yant+tamy)
 			region = im.crop(box)
 			region = region.transpose(Image.ROTATE_180)
+			mosaico_aux(region)
+			#region = mosaico_aux(region)
 			im.paste(region, box)
 			xant+=tamx
 		xant = 0
 		yant+=tamy
 	im.show()
 
-mosaico("dranzer.jpg",16,16)
+def mosaico_aux(box):
+	pixels = box.load()
+	tambox =  box.width*box.height
+	r,g,b=(0,0,0)
+	for w in range(0, box.width):
+		for h in range(0, box.height):
+			r+= box.getpixel((w,h))[0]
+			g+= box.getpixel((w,h))[1]
+			b+= box.getpixel((w,h))[2]
+	r,g,b=(r/tambox,g/tambox,b/tambox)
+	for w in range(0, box.width):
+		for h in range(0, box.height):
+			pixels[w,h] = (r,g,b)
+	
+mosaico("dranzer.jpg",10,10)
