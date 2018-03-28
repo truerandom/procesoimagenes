8feddff8e9f33f5c019995491e0815f42f210eed
chaos <chaos@localhost.localdomain>
2018-03-27 21:49:12 -0400

    letra gris

--- poc.py
+++ poc.py
@@ -49,6 +49,9 @@ class MyForm(wx.Frame):
 		# onImgLetrasGrises
 		imgletrasgrisesMenuItem = filtrosMenu.Append(wx.NewId(), "ImgLetrasGrises","Open Image")	
 		self.Bind(wx.EVT_MENU, self.onImgLetrasGrises,imgletrasgrisesMenuItem)
+		# onImgLetraGris
+		imgletragrisMenuItem = filtrosMenu.Append(wx.NewId(), "ImgLetraGris","Open Image")	
+		self.Bind(wx.EVT_MENU, self.onImgLetraGris,imgletragrisMenuItem)
 		
 		menuBar.Append(fileMenu, "&Archivos")
 		menuBar.Append(filtrosMenu, "&Filtros")
@@ -303,21 +306,41 @@ class MyForm(wx.Frame):
 		with open(self.filename+'.html', 'w') as f:
 			print >> f, htmlcode
 			print 'Archivo generado'
-		
-		"""
-		# convertimos la imagen de pil a raster
-		image = wx.EmptyImage(im.size[0], im.size[1])
-		new_image = im.convert('RGB')
-		data = new_image.tobytes()
-		image.SetData(data)
-		# fin conversion
-		fgs = wx.FlexGridSizer(cols=1, hgap=10, vgap=10)
-		sb1 = wx.StaticBitmap(self.panel, -1, wx.BitmapFromImage(image))
-		fgs.Add(sb1)
-		self.panel.SetSizerAndFit(fgs)
-		self.Fit()
-		self.Refresh()
-		"""
+			
+	def onImgLetraGris(self,event):
+		im = Image.open(self.filename)
+		pixels = im.load()
+		htmlcode='<html><head><style>body { line-height: 0; white-space: pre; font-family:Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New, monospace; }</style></head><body>\n'
+		for h in range(0, im.height):
+			for w in range(0, im.width):
+				r= im.getpixel((w,h))[0]
+				g= im.getpixel((w,h))[1]
+				b= im.getpixel((w,h))[2]
+				#actual = (data[i]*0.3 + data[i+1]*0.59 + data[i+2]*0.11) / 3
+				prom = int((r+ g+ b  )/3)
+				if 0 <= prom <= 15: htmlcode+='<font color="#000000">@</font>'
+				if 16 <= prom <= 31: htmlcode+='<font color="#101010">@</font>'
+				if 32 <= prom <= 47: htmlcode+='<font color="#202020">@</font>'
+				if 48 <= prom <= 63: htmlcode+='<font color="#303030">@</font>'
+				if 64 <= prom <= 79: htmlcode+='<font color="#404040">@</font>'
+				if 80 <= prom <= 95: htmlcode+='<font color="#505050">@</font>'
+				if 96 <= prom <= 111: htmlcode+='<font color="#606060">@</font>'
+				if 112 <= prom <= 127: htmlcode+='<font color="#696969">@</font>'
+				if 128 <= prom <= 143: htmlcode+='<font color="#787878">@</font>'
+				if 144 <= prom <= 159: htmlcode+='<font color="#909090">@</font>'
+				if 160 <= prom <= 175: htmlcode+='<font color="#A0A0A0">@</font>'
+				if 176 <= prom <= 191: htmlcode+='<font color="#B0B0B0">@</font>'
+				if 192 <= prom <= 209: htmlcode+='<font color="#C8C8C8">@</font>'
+				if 210 <= prom <= 225: htmlcode+='<font color="#D8D8D8">@</font>'
+				if 226 <= prom <= 239: htmlcode+='<font color="#E8E8E8">@</font>'
+				if 240 <= prom <= 255: htmlcode+='<font color="#F8F8F8">@</font>' 
+				if w == im.width-1:
+					htmlcode+='\n<p>'
+		htmlcode+='\n</body><html>'
+		with open(self.filename+'.html', 'w') as f:
+			print >> f, htmlcode
+			print 'Archivo generado'
+
 if __name__ == "__main__":
 	app = wx.App(False)
 	frame = MyForm().Show()
