b9376945e78f3957523231e4a7064b73bc292ad0
chaos <chaos@localhost.localdomain>
2018-05-20 01:31:53 -0400

    tarea9v2

--- tarea9.py
+++ tarea9.py
@@ -11,175 +11,52 @@ factors es una lista con el factor de amplificacion para los pixeles
 de las tuplas anteriores
 """
 def floyd(filename):
-	img = Image.open(filename).convert('LA').convert('RGB')
-	img.show()
-	pixels = img.load()
-	ancho,alto = img.width,img.height
 	idxs = [(0,1),(1,0),(1,1),(1,2)]
 	factor = 32
 	factors = [7,3,5,1]
-	for i in range(0,ancho):
-		for j in range(0,alto):
-			r,g,b = pixels[i,j]
-			# blanco 255,255,255, negro 0,0,0
-			# Si el pixel es < 128 lo cambio a negro si no a blanco mas el error
-			if r > 127: error = r - 127
-			else: error = 255 - r
-			error = int(error / factor)
-			# definimos el valor para los pixeles adyacentes (las coordenadas estan en idxs)
-			for tupidx in range(0,len(idxs)):
-				try:
-					idxi = i + int(idxs[tupidx][0])
-					idxj = j + int(idxs[tupidx][1])
-					pix = pixels[idxi,idxj]
-					nr,ng,nb = pix[0]+(error*factors[tupidx]),pix[1]+(error*factors[tupidx]),pix[2]+(error*factors[tupidx])
-					pixels[idxi,idxj] = (nr,ng,nb)
-				except Exception as e: pass
-	img.show()
+	dither(filename,factor,idxs,factors)
 
 def fakefloyd(filename):
-	img = Image.open(filename).convert('LA').convert('RGB')
-	pixels = img.load()
-	ancho,alto = img.width,img.height
-	img.show()
 	idxs = [(0,1),(1,0),(1,1)]
 	factor = 8
 	factors = [3,3,2]
-	for i in range(0,ancho):
-		for j in range(0,alto):
-			r,g,b = pixels[i,j]
-			# blanco 255,255,255, negro 0,0,0
-			# Si el pixel es < 128 lo cambio a negro si no a blanco mas el error
-			if r > 127: error = r - 127
-			else: error = 255 - r
-			error = int(error / factor)
-			# definimos el valor para los pixeles adyacentes (las coordenadas estan en idxs)
-			for tupidx in range(0,len(idxs)):
-				try:
-					idxi = i + int(idxs[tupidx][0])
-					idxj = j + int(idxs[tupidx][1])
-					pix = pixels[idxi,idxj]
-					nr,ng,nb = pix[0]+(error*factors[tupidx]),pix[1]+(error*factors[tupidx]),pix[2]+(error*factors[tupidx])
-					pixels[idxi,idxj] = (nr,ng,nb)
-				except Exception as e: pass
-	img.show()
+	dither(filename,factor,idxs,factors)
 
 def jarvis(filename):
-	img = Image.open(filename).convert('LA').convert('RGB')
-	img.show()
-	pixels = img.load()
-	ancho,alto = img.width,img.height
 	idxs = [(0,1),(0,2),(1,0),(1,1),(1,2),(1,3),(1,4)]
 	factor = 48
 	factors = [7,5,3,5,7,5,3,1,3,5,3,1]
-	for i in range(0,ancho):
-		for j in range(0,alto):
-			r,g,b = pixels[i,j]
-			# blanco 255,255,255, negro 0,0,0
-			# Si el pixel es < 128 lo cambio a negro si no a blanco mas el error
-			if r > 127: error = r - 127
-			else: error = 255 - r
-			error = int(error / factor)
-			# definimos el valor para los pixeles adyacentes (las coordenadas estan en idxs)
-			for tupidx in range(0,len(idxs)):
-				try:
-					idxi = i + int(idxs[tupidx][0])
-					idxj = j + int(idxs[tupidx][1])
-					pix = pixels[idxi,idxj]
-					nr,ng,nb = pix[0]+(error*factors[tupidx]),pix[1]+(error*factors[tupidx]),pix[2]+(error*factors[tupidx])
-					pixels[idxi,idxj] = (nr,ng,nb)
-				except Exception as e: pass
-	img.show()
+	dither(filename,factor,idxs,factors)
 
 def stucki(filename):
-	img = Image.open(filename).convert('LA').convert('RGB')
-	img.show()
-	pixels = img.load()
-	ancho,alto = img.width,img.height
 	idxs = [(0,1),(0,2),(1,0),(1,1),(1,2),(1,3),(1,4),(2,0),(2,1),(2,2),(2,3),(2,4)]
 	factor = 42
 	factors = [8,4,2,4,8,4,2,1,2,4,2,1]
-	for i in range(0,ancho):
-		for j in range(0,alto):
-			r,g,b = pixels[i,j]
-			# blanco 255,255,255, negro 0,0,0
-			# Si el pixel es < 128 lo cambio a negro si no a blanco mas el error
-			if r > 127: error = r - 127
-			else: error = 255 - r
-			error = int(error / factor)
-			# definimos el valor para los pixeles adyacentes (las coordenadas estan en idxs)
-			for tupidx in range(0,len(idxs)):
-				try:
-					idxi = i + int(idxs[tupidx][0])
-					idxj = j + int(idxs[tupidx][1])
-					pix = pixels[idxi,idxj]
-					nr,ng,nb = pix[0]+(error*factors[tupidx]),pix[1]+(error*factors[tupidx]),pix[2]+(error*factors[tupidx])
-					pixels[idxi,idxj] = (nr,ng,nb)
-				except Exception as e: pass
-	img.show()
+	dither(filename,factor,idxs,factors)
 
 def atkinson(filename):
-	img = Image.open(filename).convert('LA').convert('RGB')
-	img.show()
-	pixels = img.load()
-	ancho,alto = img.width,img.height
 	idxs = [(0,1),(0,2),(1,0),(1,1),(1,2),(2,1)]
 	factor = 8
 	factors = [1,1,1,1,1,1]
-	for i in range(0,ancho):
-		for j in range(0,alto):
-			r,g,b = pixels[i,j]
-			# blanco 255,255,255, negro 0,0,0
-			# Si el pixel es < 128 lo cambio a negro si no a blanco mas el error
-			if r > 127: error = r - 127
-			else: error = 255 - r
-			error = int(error / factor)
-			# definimos el valor para los pixeles adyacentes (las coordenadas estan en idxs)
-			for tupidx in range(0,len(idxs)):
-				try:
-					idxi = i + int(idxs[tupidx][0])
-					idxj = j + int(idxs[tupidx][1])
-					pix = pixels[idxi,idxj]
-					nr,ng,nb = pix[0]+(error*factors[tupidx]),pix[1]+(error*factors[tupidx]),pix[2]+(error*factors[tupidx])
-					pixels[idxi,idxj] = (nr,ng,nb)
-				except Exception as e: pass
-	img.show()
+	dither(filename,factor,idxs,factors)
 
 def burkes(filename):
-	img = Image.open(filename).convert('LA').convert('RGB')
-	img.show()
-	pixels = img.load()
-	ancho,alto = img.width,img.height
 	idxs = [(0,1),(0,2),(1,0),(1,1),(1,2),(1,3),(1,4)]
 	factor = 32
 	factors = [8,4,2,4,8,4,2]
-	for i in range(0,ancho):
-		for j in range(0,alto):
-			r,g,b = pixels[i,j]
-			# blanco 255,255,255, negro 0,0,0
-			# Si el pixel es < 128 lo cambio a negro si no a blanco mas el error
-			if r > 127: error = r - 127
-			else: error = 255 - r
-			error = int(error / factor)
-			# definimos el valor para los pixeles adyacentes (las coordenadas estan en idxs)
-			for tupidx in range(0,len(idxs)):
-				try:
-					idxi = i + int(idxs[tupidx][0])
-					idxj = j + int(idxs[tupidx][1])
-					pix = pixels[idxi,idxj]
-					nr,ng,nb = pix[0]+(error*factors[tupidx]),pix[1]+(error*factors[tupidx]),pix[2]+(error*factors[tupidx])
-					pixels[idxi,idxj] = (nr,ng,nb)
-				except Exception as e: pass
-	img.show()
+	dither(filename,factor,idxs,factors)
 
 def sierra(filename):
+	idxs = [(0,1),(0,2),(1,0),(1,1),(1,2),(1,3),(1,4),(2,1),(2,2),(2,3)]
+	factor = 32
+	factors = [5,3,2,4,5,4,2,2,3,2]
+	dither(filename,factor,idxs,factors)
+
+def dither(filename,factor,idxs,factors):
 	img = Image.open(filename).convert('LA').convert('RGB')
 	img.show()
 	pixels = img.load()
 	ancho,alto = img.width,img.height
-	idxs = [(0,1),(0,2),(1,0),(1,1),(1,2),(1,3),(1,4),(2,1),(2,2),(2,3)]
-	factor = 32
-	factors = [5,3,2,4,5,4,2,2,3,2]
 	for i in range(0,ancho):
 		for j in range(0,alto):
 			r,g,b = pixels[i,j]
